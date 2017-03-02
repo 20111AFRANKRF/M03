f7b330bc7e338ed40a74167eb80003ddc2217e95
Frank Rodriguez Figueroa <ffigueroa@Frank.Rodriguez>
2017-03-02 21:02:07 +0100

    python

---
+++ python/ejercicio-edad-2.py
@@ -0,0 +1,11 @@
+# coding: utf8
+
+#Si tiene entre 18 y 23 años (ambos incluídos) le decimos que puede
+#entrar en la sesión de jóvenes.
+
+edad = input("escribe tu edad: ")
+
+if edad >= 18 and edad <= 23:
+	print "Puedes entrar a la sesion de jovenes"
+else: 
+	print "No puedes entrar a la sesion de jovenes"
