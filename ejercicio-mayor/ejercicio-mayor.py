3f4e81de4d4533f62e3f3cf4eccd40fe908b27b2
ism26066750 <ism26066750@a02.informatica.escoladeltreball.org>
2017-03-16 13:05:18 +0100

    El numero mas grandegit add ejercicio-mayor/

---
+++ ejercicio-mayor/ejercicio-mayor.py
@@ -0,0 +1,11 @@
+#!/bin/python
+# coding: utf8
+num1=input("Escriba un numero: ")
+num2=input("Escriba otro numero: ")
+
+if (num1 > num2):
+	print "El numero mas grande es: ", num1
+elif (num1 == num2):
+	print "Els dos valors son iguals!"
+else:
+	print "El numero mas grande es: ", num2 
