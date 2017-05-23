85ca81ffd4b5250bbd2fcfedccf110389e9e515f
root <root@a06.informatica.escoladeltreball.org>
2017-05-24 00:01:32 +0200

    NUEVOS DATOS

---
+++ PATH-PYTHON/walk.py
@@ -0,0 +1,17 @@
+#!/bin/python
+# coding: utf-8
+for root, dirs, files in os.walk(path_to_explore):
+    for name in files:
+
+        name_path=os.path.join(root, name)
+        print(name_path)
+        print os.stat(name_path).st_size
+        print os.path.getsize(name_path)
+
+
+    for name in dirs:
+
+        name_path=os.path.join(root, name)
+        print(name_path)
+        print os.stat(name_path).st_size
+        print os.path.getsize(name_path) 
